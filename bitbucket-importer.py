3f9f1ae0a77f5089941b1ccdea344f4e7b3d50eb
Sahed <sahed.moral@123rf.com>
2023-09-28 16:08:08 +0600

    script add

--- bitbucket-importer.py
+++ bitbucket-importer.py
@@ -3,9 +3,9 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("path/to/repo")
+repo = git.Repo("https://sahedmoral@bitbucket.org/123rf-my/123rf-payments.git")
 # Your mock repo
-mock_repo = git.Repo("path/to/your/mock-repo")
+mock_repo = git.Repo("https://github.com/sahedbs23/mock-repo.git")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
