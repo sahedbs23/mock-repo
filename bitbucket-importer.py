16af940dd4a4ae7d3468162dc3fc0aa8f31ecdc6
Sahed <sahed.moral@123rf.com>
2023-09-28 16:56:33 +0600

    import commits history from 123rfapi

--- bitbucket-importer.py
+++ bitbucket-importer.py
@@ -3,11 +3,11 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("/Users/sawon/var/www/html/123rfnext")
+repo = git.Repo("/Users/sawon/var/www/html/123rfapi")
 # Your mock repo
 mock_repo = git.Repo("/Users/sawon/var/www/html/mock-repo")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
-importer.set_author(['ssawon.bs23@email.com', 'sahed.moral@123rf.com'])
+importer.set_author(['ssawon.bs23@gmail.com', 'sahed.moral@123rf.com'])
 importer.import_repository()
