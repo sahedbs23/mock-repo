d4f33e4fa3a70294574c748a49e484fe9c36cb68
Sahed <sahed.moral@123rf.com>
2023-09-28 16:14:16 +0600

    import commits history from bitbucket

--- bitbucket-importer.py
+++ bitbucket-importer.py
@@ -3,9 +3,9 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("https://sahedmoral@bitbucket.org/123rf-my/123rf-payments.git")
+repo = git.Repo("/Users/sawon/var/www/html/123rf-payments")
 # Your mock repo
-mock_repo = git.Repo("https://github.com/sahedbs23/mock-repo.git")
+mock_repo = git.Repo("/Users/sawon/var/www/html/mock-repo")
 importer = Importer([repo], mock_repo)
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github uses my personal email
